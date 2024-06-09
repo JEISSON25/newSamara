1e5ea37f38704f354ada57c8cfc8c282489ee711
INGJEISSON <jeisim18@gmail.com>
2024-06-08 19:55:41 -0500

    config

--- nginx/start-nginx.sh
+++ nginx/start-nginx.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-envsubst '${DOMAIN}' < /etc/nginx/conf.d/default.conf.template > /etc/nginx/conf.d/default.conf
-nginx -g 'daemon off;'
+envsubst '$DOMAIN' < /etc/nginx/conf.d/default.conf.template > /etc/nginx/conf.d/default.conf
+exec nginx -g 'daemon off;'
