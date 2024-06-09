e083bea5719717182035f7333c604c227b083758
INGJEISSON <jeisim18@gmail.com>
2024-06-09 15:02:28 -0500

    new configure

--- nginx/start-nginx.sh
+++ nginx/start-nginx.sh
@@ -1,5 +1,11 @@
 #!/bin/sh
 
+# Esperar hasta que los certificados estén disponibles
+while [ ! -f /etc/letsencrypt/live/${DOMAIN}/fullchain.pem ]; do
+  echo "Waiting for certificates..."
+  sleep 2
+done
+
 envsubst '${DOMAIN}' < /etc/nginx/conf.d/default.conf.template > /etc/nginx/conf.d/default.conf
 envsubst '${DOMAIN}' < /etc/nginx/conf.d/default-ssl.conf.template > /etc/nginx/conf.d/default-ssl.conf
 nginx -g 'daemon off;'
