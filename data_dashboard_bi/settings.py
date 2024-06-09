3954abc9987b7ec3252d31b0c156dc057ab6ebee
INGJEISSON <jeisim18@gmail.com>
2024-06-09 18:38:38 -0500

    init3

--- data_dashboard_bi/settings.py
+++ data_dashboard_bi/settings.py
@@ -1,13 +1,8 @@
-"""
-Django settings for data_dashboard_bi project.
-
-"""
 from pathlib import Path
 import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
@@ -16,24 +11,22 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure--tb8*f4mipg=r=*e(!&d_c4f^5ju!wtus@w*x$#rd%e0*1)y*5"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get("DJANGO_DEBUG", "")
-
-# HOST = os.environ.get("DOMAIN", "https://samara.ingejei.com")
-
+DEBUG = os.environ.get("DJANGO_DEBUG", "True") == "True"
 
 ALLOWED_HOSTS = ["*"]
 
-# CSRF_TRUSTED_ORIGINS = [
-#     HOST,
-#     "http://django",
-#     "https://anidar.inventivalab.com",
-# ]
+# CSRF settings
+CSRF_TRUSTED_ORIGINS = [
+    f"http://{os.environ.get('DOMAIN')}",
+    f"https://{os.environ.get('DOMAIN')}"
+]
 
-CSRF_TRUSTED_ORIGINS = [f'https://{os.environ.get("DOMAIN", "")}']
-
-
-# CORS
+# CORS settings
 CORS_ORIGIN_ALLOW_ALL = True
+CORS_ALLOWED_ORIGINS = [
+    f"http://{os.environ.get('DOMAIN')}",
+    f"https://{os.environ.get('DOMAIN')}"
+]
 
 CORS_EXPOSE_HEADERS = [
     "Access-Control-Allow-Origin",
@@ -41,32 +34,8 @@ CORS_EXPOSE_HEADERS = [
     "Access-Control-Allow-Methods",
     "Access-Control-Allow-Credentials",
 ]
-CORS_ORIGIN_WHITELIST = (f'https://{os.environ.get("DOMAIN", "")}')
-
-# CORS_ORIGIN_WHITELIST = (
-#     HOST,
-#     "https://app.powerbi.com",
-#     "http://app.powerbi.com",
-#     "https://content.powerapps.com",
-#     "http://content.powerapps.com",
-#     "https://anidar.inventivalab.com",
-#     "http://anidar.inventivalab.com",
-# )
-CORS_ALLOWED_ORIGINS =  [f'https://{os.environ.get("DOMAIN", "")}']
-
-# CORS_ALLOWED_ORIGINS = [
-#     HOST,
-#     "https://app.powerbi.com",
-#     "http://app.powerbi.com",
-#     "https://content.powerapps.com",
-#     "http://content.powerapps.com",
-#     "https://anidar.inventivalab.com",
-#     "http://anidar.inventivalab.com",
-# ]
-
 
 # Application definition
-
 INSTALLED_APPS = [
     "corsheaders",
     "grappelli",
@@ -76,11 +45,9 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-
     "web_app",
     "tinymce",
     "import_export",
-
     "django.contrib.sites",
     "allauth",
     "allauth.account",
@@ -96,31 +63,15 @@ LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/'
 
 SOCIALACCOUNT_PROVIDERS = {
-    # 'google': {
-    #     'SCOPE': [
-    #         'profile',
-    #         'email',
-    #     ],
-    #     'AUTH_PARAMS': {
-    #         'access_type': 'online',
-    #     }
-    # },
     'microsoft': {
         'APP': {
             'client_id': os.environ.get("MICROSOFT_CLIENT_ID", ""),
             'secret': os.environ.get("MICROSOFT_CLIENT_SECRET", ""),
             'key': ''
         },
-        'callback_url': 'https://testsamara.ingejei.com/accounts/microsoft/login/callback/',
-        # 'AUTH_PARAMS': {
-        #     'scope': 'email',
-        # },
-        # 'OAUTH_PKCE_ENABLED': True,
+        'callback_url': f"https://{os.environ.get('DOMAIN')}/accounts/microsoft/login/callback/",
     }
 }
-
-
-
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
@@ -137,12 +88,10 @@ MIDDLEWARE_CLASSES = (
     "django_user_agents.middleware.UserAgentMiddleware",
 )
 
-
 ROOT_URLCONF = "data_dashboard_bi.urls"
 
 # User model
 AUTH_USER_MODEL = "web_app.ClientUser"
-
 
 TEMPLATES = [
     {
@@ -162,40 +111,15 @@ TEMPLATES = [
 
 WSGI_APPLICATION = "data_dashboard_bi.wsgi.application"
 
-
 # Database
-# https://docs.djangoproject.com/en/4.2/ref/settings/#databases
-
-
 DATABASES = {
-        "default": {
-            "ENGINE": "django.db.backends.sqlite3",
-            "NAME": BASE_DIR / "db.sqlite3",
-        },
-    }
-# if DEBUG:
-#     DATABASES = {
-#         "default": {
-#             "ENGINE": "django.db.backends.sqlite3",
-#             "NAME": BASE_DIR / "db.sqlite3",
-#         },
-#     }
-# else:
-#     DATABASES = {
-#         "default": {
-#             "ENGINE": "django.db.backends.postgresql_psycopg2",
-#             "NAME": os.environ.get("DB_NAME", "postgres"),
-#             "USER": os.environ.get("DB_USER", "postgres"),
-#             "PASSWORD": os.environ.get("DB_PASSWORD", "postgres"),
-#             "HOST": os.environ.get("DB_HOST", "localhost"),
-#             "PORT": os.environ.get("DB_PORT", "5432"),
-#         }
-#     }
-
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "db.sqlite3",
+    },
+}
 
 # Password validation
-# https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
-
 AUTH_PASSWORD_VALIDATORS = [
     {
         "NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator",
@@ -218,22 +142,13 @@ AUTHENTICATION_BACKENDS = [
 
 SITE_ID = 1
 
-
 # Internationalization
-# https://docs.djangoproject.com/en/4.2/topics/i18n/
-
 LANGUAGE_CODE = "es-es"
-
 TIME_ZONE = "America/Bogota"
-
 USE_I18N = True
-
 USE_TZ = True
 
-
 # Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/4.2/howto/static-files/
-
 STATIC_URL = "static/"
 MEDIA_URL = "/media/"
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
@@ -243,17 +158,11 @@ STATICFILES_DIRS = [
 ]
 
 # Default primary key field type
-# https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
-
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
-
 
 # Grappelli
 GRAPPELLI_ADMIN_TITLE = os.environ.get("SITE_NAME", "Data Dashboard")
 GRAPPELLI_AUTOCOMPLETE_LIMIT = 10
-
-
-
 
 # TinyMCE
 TINYMCE_DEFAULT_CONFIG = {
@@ -264,27 +173,26 @@ TINYMCE_DEFAULT_CONFIG = {
     "selector": "textarea",
     "theme": "silver",
     "plugins": """
-            textcolor save link image media preview codesample contextmenu
-            table code lists fullscreen  insertdatetime  nonbreaking
-            contextmenu directionality searchreplace wordcount visualblocks
-            visualchars code fullscreen autolink lists  charmap print  hr
-            anchor pagebreak
-            """,
+        textcolor save link image media preview codesample contextmenu
+        table code lists fullscreen  insertdatetime  nonbreaking
+        contextmenu directionality searchreplace wordcount visualblocks
+        visualchars code fullscreen autolink lists  charmap print  hr
+        anchor pagebreak
+        """,
     "toolbar1": """
-            fullscreen preview bold italic underline | fontselect,
-            fontsizeselect  | forecolor backcolor | alignleft alignright |
-            aligncenter alignjustify | indent outdent | bullist numlist table |
-            | link image media | codesample |
-            """,
+        fullscreen preview bold italic underline | fontselect,
+        fontsizeselect  | forecolor backcolor | alignleft alignright |
+        aligncenter alignjustify | indent outdent | bullist numlist table |
+        | link image media | codesample |
+        """,
     "toolbar2": """
-            visualblocks visualchars |
-            charmap hr pagebreak nonbreaking anchor |  code |
-            """,
+        visualblocks visualchars |
+        charmap hr pagebreak nonbreaking anchor |  code |
+        """,
     "contextmenu": "formats | link image",
     "menubar": True,
     "statusbar": True,
 }
-
 
 SITE_CONTEXT = {
     "site_name": os.environ.get("SITE_NAME", "Data Dashboard"),
--- nginx/start-nginx.sh
+++ nginx/start-nginx.sh
@@ -7,5 +7,5 @@
 # done
 
 envsubst '${DOMAIN}' < /etc/nginx/conf.d/default.conf.template > /etc/nginx/conf.d/default.conf
-envsubst '${DOMAIN}' < /etc/nginx/conf.d/default-ssl.conf.template > /etc/nginx/conf.d/default-ssl.conf
+# envsubst '${DOMAIN}' < /etc/nginx/conf.d/default-ssl.conf.template > /etc/nginx/conf.d/default-ssl.conf
 nginx -g 'daemon off;'
