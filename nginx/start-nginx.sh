17c2785e35a10824497f0986f9ed9bdeba9459e3
INGJEISSON <jeisim18@gmail.com>
2024-06-09 20:54:08 -0500

    docker con ngix ssl

--- nginx/start-nginx.sh
+++ nginx/start-nginx.sh
@@ -1,13 +1,14 @@
 #!/bin/sh
 
-# Verificar que la variable DOMAIN esté definida
-if [ -z "$DOMAIN" ]; then
-  echo "Error: La variable de entorno DOMAIN no está definida."
-  exit 1
-fi
+# Asegúrate de que los certificados existan
+while [ ! -f /etc/letsencrypt/live/${DOMAIN}/fullchain.pem ]; do
+  echo "Waiting for certificates..."
+  sleep 2
+done
 
 # Reemplazar la variable de entorno en la configuración de Nginx
 envsubst '${DOMAIN}' < /etc/nginx/conf.d/default.conf.template > /etc/nginx/conf.d/default.conf
+envsubst '${DOMAIN}' < /etc/nginx/conf.d/default-ssl.conf.template > /etc/nginx/conf.d/default-ssl.conf
 
 # Iniciar Nginx
 nginx -g 'daemon off;'
