a865ff616abce08e541748cd549e7af889282f61
INGJEISSON <jeisim18@gmail.com>
2024-06-09 21:29:48 -0500

    Update start-nginx.sh

--- nginx/start-nginx.sh
+++ nginx/start-nginx.sh
@@ -1,10 +1,6 @@
 #!/bin/sh
 
 # Asegúrate de que los certificados existan
-while [ ! -f /etc/letsencrypt/live/${DOMAIN}/fullchain.pem ]; do
-  echo "Waiting for certificates..."
-  sleep 2
-done
 
 # Reemplazar la variable de entorno en la configuración de Nginx
 envsubst '${DOMAIN}' < /etc/nginx/conf.d/default.conf.template > /etc/nginx/conf.d/default.conf
