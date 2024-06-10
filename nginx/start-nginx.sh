266a202f94e918b8ee2f0749327f1efb3d410f3c
INGJEISSON <jeisim18@gmail.com>
2024-06-09 19:41:25 -0500

    config ngnix

--- nginx/start-nginx.sh
+++ nginx/start-nginx.sh
@@ -1,11 +1,13 @@
 #!/bin/sh
 
-# Esperar hasta que los certificados estén disponibles
-# while [ ! -f /etc/letsencrypt/live/${DOMAIN}/fullchain.pem ]; do
-#   echo "Waiting for certificates..."
-#   sleep 2
-# done
+# Verificar que la variable DOMAIN esté definida
+if [ -z "$DOMAIN" ]; then
+  echo "Error: La variable de entorno DOMAIN no está definida."
+  exit 1
+fi
 
+# Reemplazar la variable de entorno en la configuración de Nginx
 envsubst '${DOMAIN}' < /etc/nginx/conf.d/default.conf.template > /etc/nginx/conf.d/default.conf
-# envsubst '${DOMAIN}' < /etc/nginx/conf.d/default-ssl.conf.template > /etc/nginx/conf.d/default-ssl.conf
+
+# Iniciar Nginx
 nginx -g 'daemon off;'
